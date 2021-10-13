99d3ba40817828aff9d7bf591314ba35a7398841
Martin Lenart <martin_lenart@icloud.com>
2021-10-13 10:09:52 +0200

    Add project files.

---
+++ GroupAssignment2/DeckOfCards.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupAssignment2
+{
+    class DeckOfCards
+    {
+        const int MaxNrOfCards = 52;
+        
+        int nrOfCards = 52;
+        PlayingCard[] cards = new PlayingCard[MaxNrOfCards];
+
+        public PlayingCard this[int idx]
+        {
+            get
+            {
+                return cards[idx];
+            }
+        }
+        /// <summary>
+        /// Number of Cards in the deck
+        /// </summary>
+        public int Count => nrOfCards;
+
+        /// <summary>
+        /// Overriden and used by for example Console.WriteLine()
+        /// </summary>
+        /// <returns>string that represents the complete deck of cards</returns>
+        public override string ToString()
+        {
+            string sRet = "";
+            for (int i = 0; i < nrOfCards; i++)
+            {
+                sRet += cards[i].ToString() + "\n";
+            }
+            return sRet;
+        }
+
+        /// <summary>
+        /// Shuffles the deck of cards
+        /// </summary>
+        public void Shuffle()
+        {
+            var rnd = new Random();
+            int nrOfShuffles = rnd.Next(100, 100000);
+            for (int shuffle = 0; shuffle < nrOfShuffles; shuffle++)
+            {
+                //Swap to random cards with each other
+                int loCard = rnd.Next(0, 52);
+                int hiCard = rnd.Next(0, 52);
+
+                (cards[loCard], cards[hiCard]) = (cards[hiCard], cards[loCard]);
+            }
+        }
+        /// <summary>
+        /// Initialize a fresh deck consisting of 52 cards
+        /// </summary>
+        public void FreshDeck()
+        {
+            nrOfCards = 52;
+            int cardNr = 0;
+            for (PlayingCardColor c = PlayingCardColor.Clubs; c <= PlayingCardColor.Spades; c++)
+            {
+                for (PlayingCardValue v = PlayingCardValue.Two; v <= PlayingCardValue.Ace; v++)
+                {
+                    cards[cardNr] = new PlayingCard { Color = c, Value = v };
+                    cardNr++;
+                }
+            }
+        }
+
+        public PlayingCard GetTopCard()
+        {
+            return cards[nrOfCards-- - 1];
+        }
+
+        public DeckOfCards()
+        {
+            FreshDeck();
+        }
+    }
+}
---
+++ GroupAssignment2/PlayingCard.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupAssignment2
+{
+	public enum PlayingCardColor
+	{
+		Clubs = 0, Diamonds, Hearts, Spades         // Poker suit order, Spades highest
+	}
+	public enum PlayingCardValue
+	{
+		Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+		Knight, Queen, King, Ace                // Poker Value order
+	}
+	public class PlayingCard
+	{
+		public PlayingCardColor Color { get; init; }
+		public PlayingCardValue Value { get; init; }
+
+		public string BlackOrRed
+		{
+			get
+			{
+				if (Color == PlayingCardColor.Clubs || Color == PlayingCardColor.Spades)
+					return "Black";
+
+				return "Red";
+			}
+		}
+		/// <summary>
+		/// Returns "Face" for Ace,Knight, Queen, King. Otherwise it returns "Value".
+		/// 
+		/// </summary>
+		string FaceOrValue
+		{
+			//Use switch statment or switch expression
+			get
+			{
+				return Value switch
+				{
+					PlayingCardValue.Ace => "Face",
+					PlayingCardValue.Knight => "Face",
+					PlayingCardValue.Queen => "Face",
+					PlayingCardValue.King => "Face",
+					_ => "Value",
+				};
+			}
+		}
+		public override string ToString() => $"{Value} of {Color}, a {BlackOrRed} {FaceOrValue} card";
+
+		/// <summary>
+		/// Write a constructor that generates a random card. I.e., PlayingCard card1 = new PlayingCard() 
+		/// generates a random card.
+		/// </summary>
+		public PlayingCard() 
+		{
+			//Use the .Net class Random to generate a random number
+			var rnd = new Random();
+
+			Color = (PlayingCardColor) rnd.Next((int)PlayingCardColor.Clubs, (int) PlayingCardColor.Spades + 1);
+			Value = (PlayingCardValue)rnd.Next((int)PlayingCardValue.Two, (int)PlayingCardValue.Ace + 1);
+		}
+	}
+}
